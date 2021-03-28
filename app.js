21351af79b55bc3c82161e464ecd8bebac49e737
apotapov <apotapovxx@mail.ru>
2021-03-28 18:13:03 +0300

    refactor: rename class according to rule 3.1 in html-and-css.md

--- app.js
+++ app.js
@@ -8,7 +8,7 @@
 
 // Event handling, user interaction is what starts the code execution.
 
-var taskInput=document.getElementById("new-task");//Add a new task.
+var taskInput=document.getElementById("main__new-task-input");//Add a new task.
 var addButton=document.getElementsByTagName("button")[0];//first button
 var incompleteTaskHolder=document.getElementById("main__todo-tasks");//ul of #main__todo-tasks
 var completedTasksHolder=document.getElementById("main__completed-tasks");//main__completed-tasks
@@ -61,7 +61,7 @@ var createNewTaskElement=function(taskString){
 
 var addTask=function(){
     console.log("Add Task...");
-    //Create a new list item with the text from the #new-task:
+    //Create a new list item with the text from the #main__new-task-input:
     if (!taskInput.value) return;
     var listItem=createNewTaskElement(taskInput.value);
 
